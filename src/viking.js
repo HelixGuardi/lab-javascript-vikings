17221f406bd1316b52d1afb13141243dec72c7d1
Victor Hugo <victorhugo.pessoal.guardiola@gmail.com>
2026-01-07 18:28:49 +0000

    iteration 3 DONE + iteration BONUS 4 DONE

--- src/viking.js
+++ src/viking.js
@@ -36,7 +36,57 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage){
+    this.health -= damage;
+    if(this.health > 0){
+      return `A Saxon has received ${damage} points of damage`
+    } else if (this.health <= 0){
+      return `A Saxon has died in combat`
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+
+  addViking(vikingObj){
+    this.vikingArmy.push(vikingObj);
+  }
+
+  addSaxon(saxonObj){
+    this.saxonArmy.push(saxonObj);
+  }
+
+  vikingAttack(){
+    const lastSaxon = this.saxonArmy[this.saxonArmy.length - 1]
+    const firstViking = this.vikingArmy[0]
+
+    const vikingAtkSaxon = lastSaxon.receiveDamage(firstViking.attack());
+
+    if(lastSaxon.health <= 0){
+      this.saxonArmy.pop();
+    }
+    
+    return vikingAtkSaxon;
+  }
+
+  saxonAttack(){
+    const lastViking = this.vikingArmy[this.vikingArmy.length - 1];
+    const firstSaxon = this.saxonArmy[0];
+
+    const saxonAtkViking = lastViking.receiveDamage(firstSaxon.attack());
+
+    if(lastViking.health <= 0){
+      this.vikingArmy.pop();
+    }
+
+    return saxonAtkViking;
+  }
+
+  showStatus(){}
+}
